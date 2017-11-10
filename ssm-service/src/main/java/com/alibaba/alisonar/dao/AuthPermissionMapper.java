2145081f8ea6f3ac049a56fa7af6ad27ae2bdea7
wb-zxx263018 <wb-zxx263018@alibaba-inc.com>
2017-11-10 17:55:24 +0800

    msg-RBAC权限管理

--- ssm-service/src/main/java/com/alibaba/alisonar/dao/AuthPermissionMapper.java
+++ ssm-service/src/main/java/com/alibaba/alisonar/dao/AuthPermissionMapper.java
@@ -1,28 +1,15 @@
 package com.alibaba.alisonar.dao;
 
 import com.alibaba.alisonar.domain.AuthPermission;
-import com.alibaba.alisonar.domain.AuthPermissionExample;
-import java.util.List;
-import org.apache.ibatis.annotations.Param;
 
 public interface AuthPermissionMapper {
-    long countByExample(AuthPermissionExample example);
-
-    int deleteByExample(AuthPermissionExample example);
-
     int deleteByPrimaryKey(Long id);
 
     int insert(AuthPermission record);
 
     int insertSelective(AuthPermission record);
 
-    List<AuthPermission> selectByExample(AuthPermissionExample example);
-
     AuthPermission selectByPrimaryKey(Long id);
-
-    int updateByExampleSelective(@Param("record") AuthPermission record, @Param("example") AuthPermissionExample example);
-
-    int updateByExample(@Param("record") AuthPermission record, @Param("example") AuthPermissionExample example);
 
     int updateByPrimaryKeySelective(AuthPermission record);
 
