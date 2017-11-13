7606abaac87a63e17bdd36cbaebf98d82452b030
wb-zxx263018 <wb-zxx263018@alibaba-inc.com>
2017-11-13 17:22:20 +0800

    msg-登录

---
+++ ssm-web/src/main/webapp/WEB-INF/assets/js/app_tools.js
@@ -0,0 +1,17 @@
+$.NOTIFY = {
+	showNotify : function(title , text ,type){
+		var title = title || "Done" ;
+		var text = text || "操作成功" ;
+		var type = type || "success"
+		PNotify.removeAll(); //删除其他通知
+		new PNotify({
+		    title: title,
+			styling : 'bootstrap3',
+		    text: text ,
+		    type: type ,
+			delay:1000
+		});
+		
+	},
+		
+};
